df5f854c3d58241af05203bd34a91d5e6d5b835b
ej68 <ej68@duke.edu>
2019-03-06 20:25:46 -0500

    year to congress and back

--- HouseElections/app.R
+++ HouseElections/app.R
@@ -5,7 +5,14 @@ library(sf)
 library(shinydashboard)
 library(readr)
 
-districts = load("../Data/shapes.Rdata")
+# Silly functions to easily convert between years <-> congress numbers
+year_to_congress = function(year){
+  floor((year - 1789)/2 + 1)
+}
+congress_to_election = function(congress){
+  2 * congress + 1786
+}
+
 elections = read_csv("Data/house.csv")
 
 ui = dashboardPage(
